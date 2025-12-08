e668d5a4b923ce579c3365edade74aa19d3e97c5
Karen Tano <thetanos24@gmail.com>
2025-12-08 16:15:27 -0700

    updated review edits

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,12 +3,8 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <div>
-        <p className="footer__author">Developed by Karen Tano</p>
-      </div>
-      <div>
-        <p className="footer__year">2025</p>
-      </div>
+      <p className="footer__author">Developed by Karen Tano</p>
+      <p className="footer__year">2025</p>
     </footer>
   );
 }
