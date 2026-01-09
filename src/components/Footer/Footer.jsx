779f4a7499584e90e4959347f2946bf163347a33
Karen Tano <thetanos24@gmail.com>
2026-01-09 00:02:27 -0700

    updated footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="footer__author">Developed by Karen Tano</p>
-      <p className="footer__year">2025</p>
+      <p className="footer__year">2026</p>
     </footer>
   );
 }
