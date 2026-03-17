56191a27375e5ef84b78bb98740f905f0303fa6f
Karen Tano <thetanos24@gmail.com>
2026-03-17 16:48:40 -0700

    fixed delete failure on item modal

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -5,10 +5,8 @@ import CurrentUserContext from "../../contexts/CurrentUserContext";
 
 function ItemModal({ isOpen, card, onClose, name, onDelete }) {
   const currentUser = useContext(CurrentUserContext);
+
   const isOwn = card.owner === currentUser?._id;
-  const itemDeleteButtonClassName = `modal__delete-btn ${
-    isOwn ? "modal__delete-btn_visible" : "modal__delete-btn_hidden"
-  }`;
 
   const handleOverlayClick = (e) => {
     if (e.target === e.currentTarget) {
@@ -35,13 +33,16 @@ function ItemModal({ isOpen, card, onClose, name, onDelete }) {
           <div className="modal__description">
             <div className="modal__header-row">
               <p className="modal__caption">{card.name}</p>
-              <button
-                type="button"
-                className={itemDeleteButtonClassName}
-                onClick={() => onDelete(card)}
-              >
-                Delete item
-              </button>
+
+              {isOwn && (
+                <button
+                  type="button"
+                  className="modal__delete-btn"
+                  onClick={() => onDelete(card)}
+                >
+                  Delete item
+                </button>
+              )}
             </div>
           </div>
           <p className="modal__weather">Weather: {card.weather}</p>
